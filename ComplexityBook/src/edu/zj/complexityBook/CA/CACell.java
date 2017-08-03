953bebd6a531ca9f28ececb5a02ef37cbbf22591
ZHANG Jun <zhangjun@cueb.edu.cn>
2017-08-03 20:55:19 +0800

    add forest fire model

--- ComplexityBook/src/edu/zj/complexityBook/CA/CACell.java
+++ ComplexityBook/src/edu/zj/complexityBook/CA/CACell.java
@@ -1,24 +1,61 @@
 package edu.zj.complexityBook.CA;
 
-import edu.zj.utils.Grid.Grid;
+import java.util.ArrayList;
+import java.util.List;
 
-public abstract class CACell<S, G extends Grid<S>> {
-	protected G caGrid;
+import edu.zj.utils.Grid.Grid;
+import edu.zj.utils.Grid.Grid.Pos;
+
+public abstract class CACell<S, M extends CAMain<S, ? extends Grid<S>, ? extends CACell<S, M>>> {
+	protected M caMain;
 	protected int row;
 	protected int column;
 	protected S state;
 	protected S nextState;
 
-	public CACell(G caGrid, int row, int column, S state) {
-		super();
-		this.caGrid = caGrid;
+	public S getNextState() {
+		return nextState;
+	}
+
+	public void setNextState(S nextState) {
+		this.nextState = nextState;
+	}
+
+	public CACell(M caMain, int row, int column, S state) {
+		this.caMain = caMain;
 		this.row = row;
 		this.column = column;
 		this.state = state;
 	}
 
+	public CACell(M caMain, int row, int column) {
+		this.caMain = caMain;
+		this.row = row;
+		this.column = column;
+		this.state = caMain.getCaGrid().get(row, column);
+	}
+
+	public CACell(M caMain, Pos pos) {
+		this.caMain = caMain;
+		this.row = pos.getRow();
+		this.column = pos.getColumn();
+		this.state = caMain.getCaGrid().get(pos);
+	}
+
 	public S getState() {
 		return state;
+	}
+
+	public List<CACell<S, M>> neighbs4() {
+		List<Pos> pos = caMain.getCaGrid().neighbs4(row, column);
+		List<CACell<S, M>> list = new ArrayList<>();
+		CACell<S, M> cell;
+		for (Pos p : pos) {
+			cell = caMain.getCellMap().get(p);
+			if (cell != null)
+				list.add(cell);
+		}
+		return list;
 	}
 
 	public int getRow() {
@@ -45,8 +82,9 @@ public abstract class CACell<S, G extends Grid<S>> {
 
 	public void update() {
 		if (nextState != null) {
-			caGrid.set(row, column, nextState);
+			caMain.getCaGrid().set(row, column, nextState);
 			state = nextState;
+			nextState = null;
 		}
 	}
 
