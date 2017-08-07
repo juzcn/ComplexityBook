b529116d8535cd75f97594b32772c3ba00ef2dac
ZHANG Jun <zhangjun@cueb.edu.cn>
2017-08-07 15:46:09 +0800

    continue updates

--- ComplexityBook/src/edu/zj/complexityBook/CA/Wolfram/WolframView.java
+++ ComplexityBook/src/edu/zj/complexityBook/CA/Wolfram/WolframView.java
@@ -3,19 +3,44 @@ package edu.zj.complexityBook.CA.Wolfram;
 import edu.zj.complexityBook.CA.Wolfram.WolframCell.State;
 import edu.zj.utils.Grid.Model.ArrayGrid;
 import edu.zj.utils.Grid.View.GridDisplayCanvas;
+import javafx.scene.Node;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 //public class WolframView extends CAGridView<State,Integer,WolframData> {
-	public class WolframView extends GridDisplayCanvas<WolframCell.State,ArrayGrid<WolframCell.State>> {
+public class WolframView extends GridDisplayCanvas<WolframCell.State, ArrayGrid<WolframCell.State>> {
+	public WolframView(WolframMain model, GridViewParams params ) {
+		super(1, model.getColumnCount(), params.getCellSize(), Color.BLACK, model.getCaGrid());
+		VBox vbox = new VBox();
+		vbox.setPrefHeight(600);
+		vbox.getChildren().add(super.getNode());
+		setNode(vbox);
+		setTitle("Wolfram CA ±ø’Õº");
+	}
 
-	public WolframView(int columnCount, double latticeSize,ArrayGrid<WolframCell.State> caGrid) {
-		super(1, columnCount, latticeSize,Color.BLACK,caGrid);
+	
+	@Override
+	public Color cellFill(State data) {
+		if (data == WolframCell.State.ON)
+			return Color.BLACK;
+		return Color.WHITE;
 	}
 
 	@Override
-	public Color cellFill(State data) {
-		if (data== WolframCell.State.ON) return Color.BLACK;
-		return Color.WHITE;
+	public void redraw() {
+		Canvas c = new Canvas(getMinColumnCount() * this.getCellSize(), getCellSize());
+		((VBox)super.getNode()).getChildren().add(c);
+		this.setGridGC(c.getGraphicsContext2D());
+		updateViewPort();
+		super.show();
+	}
+
+
+	@Override
+	public Node getNode() {
+		return new ScrollPane(super.getNode());
 	}
 
 }
